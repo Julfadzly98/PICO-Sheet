4d10058ebe63e37a09e23b56fd179a31fefd743e
julfadzlymuhaimin98 <57395950+Julfadzly98@users.noreply.github.com>
2023-11-15 20:19:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from time import sleep
 import urequests
 
 x = 7
+y = 9
 
 def connect():
     import network
  
-    ssid = "Staff KML" 
-    password = "stafkml@15" 
+    ssid = "WIFI-NAME" 
+    password = "PASSWORD" 
  
     station = network.WLAN(network.STA_IF)
  
@@ -27,16 +28,14 @@ def connect():
 connect()
 
 while True:
-    sleep(5)
+    sleep(2)
     
     h = {'content-type' : 'application/x-www-form-urlencoded'}
     form_url = 'https://docs.google.com/forms/d/e/1FAIpQLSfTfxMNKdGAepcyGISCoz6ynsDEdTJy9JiGha_HL5ypHgSg-g/formResponse?usp=pp_url&'
-    form_data = 'entry.1234193767=' + str(x)
+    form_data = 'entry.1234193767=' + str(x) + '&entry.1555660614=' + str(y)
     r = urequests.post(form_url, data=form_data, headers=h)
     r.status_code
     r.close()
     print("check google form")
     
-    
-    
-#https://docs.google.com/forms/d/e/1FAIpQLSfTfxMNKdGAepcyGISCoz6ynsDEdTJy9JiGha_HL5ypHgSg-g/formResponse?usp=pp_url&entry.1234193767={}&submit=Submit
+    #https://docs.google.com/forms/d/e/1FAIpQLSfTfxMNKdGAepcyGISCoz6ynsDEdTJy9JiGha_HL5ypHgSg-g/viewform?usp=pp_url&entry.1234193767=20&entry.1555660614=20
